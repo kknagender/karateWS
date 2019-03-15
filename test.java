9c2ea4b0aab442aed9a27e5944f53732408aa17c
nkairamkonda <nkairamkonda@01LTP0106.csilhyd1.com>
2019-03-15 19:17:00 +0530

    commit 2

---
+++ test.java
@@ -0,0 +1,14 @@
+package com.intuit.karate.karate_archetype;
+
+import org.junit.runner.RunWith;
+import org.junit.Test;
+
+import com.intuit.karate.*;
+
+@RunWith(Karate.class)
+public class test {
+
+
+	}
+
+
